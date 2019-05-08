09d06019c6bccf51964c39968a5f0eedbc58f5e0
mackerel <274336317@qq.com>
2019-05-08 16:49:53 +0800

    添加界面

---
+++ iot-data-portal/src/main/java/com/mackerelpike/springcloud/iot_data_portal/SpringCloudDataPortalApplication.java
@@ -0,0 +1,13 @@
+package com.mackerelpike.springcloud.iot_data_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudDataPortalApplication 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(SpringCloudDataPortalApplication.class, args);
+    }
+}
